d8fd933f3a363546fb93994f21eb5ba1fc573f4b
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-14 16:26:13 +0530

    Feature Order Service : Adding controller layer for order feature

---
+++ order-service/src/main/java/com/learning/orderservice/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.learning.orderservice.controller;
+
+import com.learning.orderservice.model.CalPriceResponse;
+import com.learning.orderservice.service.serviceImpl.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    OrderServiceImpl orderServiceImpl;
+
+    @GetMapping("/calculatePrice")
+    public ResponseEntity<?> calculatePrice(@RequestParam("ItemId") String ItemId, @RequestParam("quantity") int quantity) {
+        CalPriceResponse calPriceResponse = orderServiceImpl.calculatePriceService(ItemId, quantity);
+        return new ResponseEntity<>(calPriceResponse, HttpStatus.OK);
+
+    }
+}
