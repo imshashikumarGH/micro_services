e9477898dba6a822e8238ddf2843adacb5993736
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-14 22:10:38 +0530

    Feature Product Service : Adding new product repo

--- order-service/src/main/java/com/learning/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/learning/orderservice/OrderServiceApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
