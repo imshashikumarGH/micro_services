5ecc8dafb21c3eb845cf264e8006df7361ed99c7
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-15 20:37:43 +0530

    Feature Product Service : Adding model class

---
+++ product-service/src/main/java/com/learning/productservice/model/GetPriceResponse.java
@@ -0,0 +1,13 @@
+package com.learning.productservice.model;
+
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetPriceResponse {
+    private String productId;
+    private int price;
+}
