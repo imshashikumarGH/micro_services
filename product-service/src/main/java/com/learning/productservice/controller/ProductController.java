c0ae33b7051db649d1d3add67d16677c94959df4
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-16 21:59:48 +0530

    Feature Swagger UI Product Service : Adding maven dependencies for swagger UI and code changes

--- product-service/src/main/java/com/learning/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/learning/productservice/controller/ProductController.java
@@ -5,12 +5,13 @@ import com.learning.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 @RequestMapping("/product")
 public class ProductController {
 
