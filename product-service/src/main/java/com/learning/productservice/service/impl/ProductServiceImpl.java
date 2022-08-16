59a0bcdb0a0ed5cf80ec69b1494d13b10b400ba4
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-16 20:33:40 +0530

    Feature Product Service : Auto wiring dao layer

--- product-service/src/main/java/com/learning/productservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/learning/productservice/service/impl/ProductServiceImpl.java
@@ -1,13 +1,19 @@
 package com.learning.productservice.service.impl;
 
+import com.learning.productservice.dao.ProductDao;
 import com.learning.productservice.model.GetPriceResponse;
 import com.learning.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    ProductDao productDao;
+
     @Override
     public GetPriceResponse getPriceForProductService(String productId) {
-        return GetPriceResponse.builder().price(100).productId(productId).build();
+        return productDao.getPriceForProduct(productId);
     }
 }
