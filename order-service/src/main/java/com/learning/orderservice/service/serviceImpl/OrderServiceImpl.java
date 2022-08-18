08c05b4b81dcac8e9fcc09d3eaf9a02fec928a92
imshashikumarGH <imshashikumar01@gmail.com>
2022-08-18 22:44:39 +0530

    Feature Feign : Auto-wiring to call product backend service

--- order-service/src/main/java/com/learning/orderservice/service/serviceImpl/OrderServiceImpl.java
+++ order-service/src/main/java/com/learning/orderservice/service/serviceImpl/OrderServiceImpl.java
@@ -1,8 +1,35 @@
 package com.learning.orderservice.service.serviceImpl;
 
+import com.learning.orderservice.backendServices.ProductBackendService;
+import com.learning.orderservice.dao.Impl.OrderDaoImpl;
 import com.learning.orderservice.model.CalPriceResponse;
+import com.learning.orderservice.model.GetPriceResponse;
+import com.learning.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-public interface OrderServiceImpl {
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    OrderDaoImpl orderDaoImpl;
 
-    CalPriceResponse calculatePriceService(String itemId, int quantity);
+    @Autowired
+    ProductBackendService productBackendService;
+
+    @Override
+    public CalPriceResponse calculatePriceService(String itemId, int quantity) {
+        ResponseEntity<GetPriceResponse> priceForProductServices = productBackendService.getPriceForItem(itemId);
+        if (priceForProductServices.getStatusCode().is2xxSuccessful()) {
+            int price = priceForProductServices.getBody().getPrice();
+            CalPriceResponse calPriceResponse = CalPriceResponse.builder().totalPrice(price * quantity).quantity(quantity).itemId(itemId).build();
+            return calPriceResponse;
+        }
+        int totalPrice = orderDaoImpl.calculatePriceDao(itemId, quantity);
+        return CalPriceResponse.builder()
+                .itemId(itemId)
+                .totalPrice(totalPrice)
+                .quantity(quantity).build();
+    }
+
 }
